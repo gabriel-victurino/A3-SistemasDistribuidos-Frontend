76121ab4b229b53acc43f21930feb0ee9b2f0b8e
joao-fortkamp06 <joaofort37@gmail.com>
2025-11-10 21:23:02 -0300

    Feat: Adicionado RemoteHello.java

---
+++ src/main/java/principal/RemoteHello.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package principal;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface RemoteHello extends Remote {
+
+    public String getMensagem() throws RemoteException;
+
+    public void setMensagem(String mensagem) throws RemoteException;
+}
